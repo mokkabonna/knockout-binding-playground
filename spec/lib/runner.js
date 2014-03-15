4d660804fd586a8f848cd1964f7ea1874a3affcc
Martin Hansen <martin@martinhansen.no>
2014-03-15 11:51:53 +0100

    not in generator

--- spec/lib/runner.js
+++
@@ -1,8 +0,0 @@
-require.config({
-  baseUrl: 'base/', //karma servers files from base
-  paths: {
-    knockout: 'bower_components/knockout.js/knockout'
-  }
-});
-
-require(['helper', 'tests/hidden'], window.__karma__.start);
